a9acf7782b82ca06a84b86bab79e0bb59df8cdbe
chloe alcorn <ca.alcorn@outlook.com>
2018-10-16 11:38:07 +0100

    initial commit

---
+++ src/main/java/Radio.java
@@ -0,0 +1,15 @@
+public class Radio {
+
+    private String station;
+
+    public Radio(String station) {
+        this.station = station;
+
+
+    }
+
+
+    public String tune() {
+        return this.station;
+    }
+}
